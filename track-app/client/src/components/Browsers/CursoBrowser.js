955c19dec3c98ee60f8faad7137323cc22c98751
Sebastian <sebyra@gmail.com>
2019-09-08 11:37:00 -0300

    Acomodado el layout de cursos

--- track-app/client/src/components/Browsers/CursoBrowser.js
+++ track-app/client/src/components/Browsers/CursoBrowser.js
@@ -3,8 +3,7 @@ import { Table } from "reactstrap";
 
 import api from "../Api/apiRar";
 import CursoRow from "../Rows/CursoRow";
-import NuevoCurso from "../Collapses/NuevoCurso"
-
+import NuevoCurso from "../Collapses/NuevoCurso";
 
 export default class CursoBrowser extends React.Component {
   constructor() {
@@ -31,37 +30,55 @@ export default class CursoBrowser extends React.Component {
   render() {
     const { data } = this.state;
     return (
-      <Table>
-        <thead>
-          <div>
-          <th> <NuevoCurso addFn={()=> this.add()}/></th>
-          </div>
+      <div className="container">
+        <div className="row">
+          <NuevoCurso addFn={() => this.add()} />
+        </div>
 
-          <tr>
-            <th>Materia</th>
-            <th>Sede</th>
-            <th>Dias</th>
-            <th>Cuatrimestre</th>
-            <th>Año</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
+        <div className="row">
+          <Table striped>
+            <thead>
+              <tr>
+                <div className="row">
+                  <div className="col-1">
+                    <th>Materia</th>
+                  </div>
+                  <div className="col-2">
+                    <th>Sede</th>
+                  </div>
+                  <div className="col-2">
+                    <th>Dias</th>
+                  </div>
+                  <div className="col-2">
+                    <th>Cuatrimestre</th>
+                  </div>
+                  <div className="col-1">
+                    <th>Año</th>
+                  </div>
+                  <div className="col-4">
+                    <th>Acciones</th>
+                  </div>
+                </div>
+              </tr>
+            </thead>
 
-        {data.map((curso, idx) => (
-          <CursoRow
-            key={idx}
-            indice={idx}
-            materia={curso.materia}
-            sede={curso.sede}
-            dias={curso.dias}
-            cuatrimestre={curso.cuatrimestre}
-            anio={curso.anio}
-            id={curso._id}
-            callbackFn={id => this.delete(id)}
-            cbAdd={id => this.add()}
-          />
-        ))}
-      </Table>
+            {data.map((curso, idx) => (
+              <CursoRow
+                key={idx}
+                indice={idx}
+                materia={curso.materia}
+                sede={curso.sede}
+                dias={curso.dias}
+                cuatrimestre={curso.cuatrimestre}
+                anio={curso.anio}
+                id={curso._id}
+                callbackFn={id => this.delete(id)}
+                cbAdd={id => this.add()}
+              />
+            ))}
+          </Table>
+        </div>
+      </div>
     );
   }
 }
--- track-app/client/src/components/Rows/CursoRow.js
+++ track-app/client/src/components/Rows/CursoRow.js
@@ -1,59 +1,63 @@
 import React, { Component } from "react";
-import { Button,ButtonGroup} from "reactstrap";
-import EditarCurso from "../Collapses/EditarCurso"
-import {FaArrowAltCircleRight, FaTrash } from 'react-icons/fa';
+import { Button, ButtonGroup } from "reactstrap";
+import EditarCurso from "../Collapses/EditarCurso";
+import { FaArrowAltCircleRight, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
-
 
 class CursoRow extends Component {
   render() {
     return (
       <tbody>
         <tr key={this.props.indice}>
-          <td id="materia">{this.props.materia}</td>
-          <td id="sede">{this.props.sede}</td>
-          <td id="dias">{this.props.dias}</td>
-          <td id="cuatrimestre">{this.props.cuatrimestre}</td>
-          <td id="anio">{this.props.anio}</td>
-
-          <td>
-          
-          <ButtonGroup> 
-            <EditarCurso {...this.props} />
-            <div>
-              <Button
-                className="Delete-Button"
-                color="danger"
-                size="sm"
-                onClick={e => {
-                  if (
-                    window.confirm(
-                      "¿Esta seguro que desea eliminar este curso?"
-                    )
-                  )
-                  this.props.callbackFn(this.props.id);;
-                }}
-              >
-                <FaTrash />
-             
-              </Button>
-
-              </div>
-              <div>
-              <Link
-              
-              to={"/curso/" + this.props.id}
-              className="btn btn-primary Edit-Button btn-sm"
-              
-              
-            >
-               <FaArrowAltCircleRight/>
-               
-               </Link>
-               </div>
-               </ButtonGroup>
-          </td>
+          <div className="row">
+            <div className="col-1">
+              <td id="materia">{this.props.materia}</td>
+            </div>
+            <div className="col-2">
+              <td id="sede">{this.props.sede}</td>
+            </div>
+            <div className="col-2">
+              <td id="dias">{this.props.dias}</td>
+            </div>
+            <div className="col-2">
+              <td id="cuatrimestre">{this.props.cuatrimestre}</td>
+            </div>
+            <div className="col-1">
+              <td id="anio">{this.props.anio}</td>
+            </div>
+            <div className="col-4">
+              <td>
+                <ButtonGroup>
+                  <EditarCurso {...this.props} />
+                  <div>
+                    <Button
+                      className="Delete-Button"
+                      color="danger"
+                      size="sm"
+                      onClick={e => {
+                        if (
+                          window.confirm(
+                            "¿Esta seguro que desea eliminar este curso?"
+                          )
+                        )
+                          this.props.callbackFn(this.props.id);
+                      }}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </div>
+                  <div>
+                    <Link
+                      to={"/curso/" + this.props.id}
+                      className="btn btn-primary Edit-Button btn-sm"
+                    >
+                      <FaArrowAltCircleRight />
+                    </Link>
+                  </div>
+                </ButtonGroup>
+              </td>
+            </div>
+          </div>
         </tr>
       </tbody>
     );
